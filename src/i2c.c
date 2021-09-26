20b38984d80fc7e5ec953f81982e75921ccb8171
aikawaYO <mekonoku@yahoo.co.jp>
2021-09-26 18:10:00 +0900

    fix i2c.c 4 -> 2space

--- src/i2c.c
+++ src/i2c.c
@@ -5,7 +5,7 @@
 void
 c_i2c_init(mrb_vm *vm, mrb_value *v, int argc)
 {
-	i2c_init(I2C_PORT, 400 * 1000);
+  i2c_init(I2C_PORT, 400 * 1000);
   gpio_set_function(16, GPIO_FUNC_I2C);
   gpio_set_function(17, GPIO_FUNC_I2C);
   gpio_pull_up(16);
@@ -15,23 +15,23 @@ c_i2c_init(mrb_vm *vm, mrb_value *v, int argc)
 void
 c_i2c_write(mrb_vm *vm, mrb_value *v, int argc)
 {
-	int i;
-	uint8_t slave_addr = GET_INT_ARG(1);
+  int i;
+  uint8_t slave_addr = GET_INT_ARG(1);
   uint8_t n = mrbc_array_size(&v[2]);
   uint8_t datas[n];
-	for(int i=0;i < n;i++){
-	  datas[i] = mrbc_array_get(&v[2] ,i).i;
-	}
+  for(int i=0;i < n;i++){
+    datas[i] = mrbc_array_get(&v[2] ,i).i;
+  }
   i2c_write_blocking(I2C_PORT, slave_addr, datas, n, false);
 }
 
 void
 c_i2c_read(mrb_vm *vm, mrb_value *v, int argc)
 {
-	uint8_t slave_addr = GET_INT_ARG(1);
-	uint8_t rom_addr = GET_INT_ARG(2);
-	uint8_t buffer_num = GET_INT_ARG(3);
-	uint8_t buffer[buffer_num];
+  uint8_t slave_addr = GET_INT_ARG(1);
+  uint8_t rom_addr = GET_INT_ARG(2);
+  uint8_t buffer_num = GET_INT_ARG(3);
+  uint8_t buffer[buffer_num];
   i2c_write_blocking(I2C_PORT, slave_addr, &rom_addr, 1, true);
   i2c_read_blocking(I2C_PORT, slave_addr, buffer, buffer_num, false);
 
