9fb6f0bb0de6a8af55e56d4dd2eb3a2f834efb02
kazhida <kazhida@abplus.com>
2021-10-08 14:29:33 +0900

    fixed indent and more.

--- src/ruby/app/models/keyboard.rb
+++ src/ruby/app/models/keyboard.rb
@@ -175,41 +175,41 @@ class Keyboard
           if key_name == key_symbol
             # @type var on_release_action: Integer | Array[Integer] | Proc | nil
             on_release_action = case on_release.class
-                                when Symbol
-                                  # @type var on_release: Symbol
-                                  key = KC_ALIASES[on_release] ? KC_ALIASES[on_release] : on_release
-                                  keycode_index = KEYCODE.index(key)
-                                  if keycode_index
-                                    keycode_index * -1
-                                  elsif KEYCODE_SFT[key]
-                                    (KEYCODE_SFT[key] + 0x100) * -1
-                                  end
-                                when Array
-                                  # @type var on_release: Array[Symbol]
-                                  # @type var ary: Array[Integer]
-                                  ary = Array.new
-                                  on_release.each do |sym|
-                                    keycode_index = KEYCODE.index(sym)
-                                    ary << if keycode_index
-                                             keycode_index * -1
-                                           elsif KEYCODE_SFT[sym]
-                                             (KEYCODE_SFT[sym] + 0x100) * -1
-                                           else # Should be a modifier
-                                             MOD_KEYCODE[sym]
-                                           end
-                                  end
-                                  ary
-                                when Proc
-                                  # @type var on_release: Proc
-                                  on_release
-                                end
+              when Symbol
+                # @type var on_release: Symbol
+                key = KC_ALIASES[on_release] ? KC_ALIASES[on_release] : on_release
+                keycode_index = KEYCODE.index(key)
+                if keycode_index
+                   keycode_index * -1
+                elsif KEYCODE_SFT[key]
+                  (KEYCODE_SFT[key] + 0x100) * -1
+                end
+              when Array
+                # @type var on_release: Array[Symbol]
+                # @type var ary: Array[Integer]
+                ary = Array.new
+                on_release.each do |sym|
+                  keycode_index = KEYCODE.index(sym)
+                  ary << if keycode_index
+                    keycode_index * -1
+                  elsif KEYCODE_SFT[sym]
+                    (KEYCODE_SFT[sym] + 0x100) * -1
+                  else # Should be a modifier
+                    MOD_KEYCODE[sym]
+                  end
+                end
+                ary
+              when Proc
+                # @type var on_release: Proc
+                on_release
+              end
             on_hold_action = if on_hold.is_a?(Symbol)
-                               # @type var on_hold: Symbol
-                               MOD_KEYCODE[on_hold] ? MOD_KEYCODE[on_hold] : on_hold
-                             else
-                               # @type var on_hold: Proc
-                               on_hold
-                             end
+              # @type var on_hold: Symbol
+              MOD_KEYCODE[on_hold] ? MOD_KEYCODE[on_hold] : on_hold
+            else
+              # @type var on_hold: Proc
+              on_hold
+            end
             @mode_keys << {
               layer:             layer,
               on_release:        on_release_action,
@@ -224,35 +224,6 @@ class Keyboard
           end
         end
       end
-    end
-  end
-
-  def action_on_release(mode_key)
-    case mode_key.class
-    when Integer
-      # @type var mode_key: Integer
-      if mode_key < -255
-        @keycodes << ((mode_key + 0x100) * -1).chr
-        @modifier |= 0b00000010
-      else
-        @keycodes << (mode_key * -1).chr
-      end
-    when Array
-      0 # `steep check` will fail if you remove this line 🤔
-      # @type var mode_key: Array[Integer]
-      mode_key.each do |key|
-        if key < -255
-          @keycodes << ((key + 0x100) * -1).chr
-          @modifier |= 0b00000010
-        elsif key < 0
-          @keycodes << (key * -1).chr
-        else # Should be a modifier
-          @modifier |= key
-        end
-      end
-    when Proc
-      # @type var mode_key: Proc
-      mode_key.call
     end
   end
 
@@ -314,6 +285,35 @@ class Keyboard
   def keys_include?(key)
     keycode = KEYCODE.index(key)
     !keycode.nil? && @keycodes.include?(keycode.chr)
+  end
+
+  def action_on_release(mode_key)
+    case mode_key.class
+    when Integer
+      # @type var mode_key: Integer
+      if mode_key < -255
+        @keycodes << ((mode_key + 0x100) * -1).chr
+        @modifier |= 0b00000010
+      else
+        @keycodes << (mode_key * -1).chr
+      end
+    when Array
+      0 # `steep check` will fail if you remove this line 🤔
+      # @type var mode_key: Array[Integer]
+      mode_key.each do |key|
+        if key < -255
+          @keycodes << ((key + 0x100) * -1).chr
+          @modifier |= 0b00000010
+        elsif key < 0
+          @keycodes << (key * -1).chr
+        else # Should be a modifier
+          @modifier |= key
+        end
+      end
+    when Proc
+      # @type var mode_key: Proc
+      mode_key.call
+    end
   end
 
   # **************************************************************
