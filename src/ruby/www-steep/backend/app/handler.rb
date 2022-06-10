4d8f05204531197436e6b66c233644b706ba317c
HASUMI Hitoshi <hasumikin@gmail.com>
2022-06-10 11:34:33 +0900

    almost works

--- src/ruby/www-steep/backend/app/handler.rb
+++ src/ruby/www-steep/backend/app/handler.rb
@@ -1,8 +1,11 @@
 # frozen_string_literal: true
 
 require "json"
-require "open3"
 require "fileutils"
+
+require 'pathname'
+require 'steep'
+require 'steep/cli'
 
 def hello(event:, context:)
   {
@@ -26,21 +29,53 @@ def steep(event:, context:)
   if MAX_KEYMAP_SIZE * 1024 < FileTest.size("/tmp/steep/keymap.rb")
     {
       statusCode: 200,
+      headers: headers,
       body: {
         message: "keymap.rb is too big. It should be less than #{MAX_KEYMAP_SIZE} KB.",
       }.to_json
     }
   else
-    result, _e, status = FileUtils.cd "/tmp/steep" do
-      Open3.capture3("steep check")
-    end
+    result = _steep_check
     {
       statusCode: 200,
+      headers: headers,
       body: {
         message: result,
       }.to_json
     }
   end
+end
+
+def _steep_check
+  begin
+    stdout = File.open("/tmp/steep/stdout", "w+")
+    argv = [
+      "check",
+      "--steepfile=/tmp/steep/Steepfile",
+    ]
+    if ENV['STAGE'] == "prod"
+      argv << "--steep-command=#{ENV['GEM_PATH']}/bin/steep"
+    end
+    Steep::CLI.new(argv: argv, stdout: stdout, stderr: STDERR, stdin: STDIN).run
+  rescue => exn
+    STDERR.puts exn.inspect
+    exn.backtrace.each do |t|
+      STDERR.puts "  #{t}"
+    end
+    exit 2
+  ensure
+    stdout.rewind
+    result = stdout.read
+    stdout.close
+    return result
+  end
+end
+
+def headers
+  {
+    "Access-Control-Allow-Origin" => "*",
+    "Access-Control-Allow-Credentials" => true
+  }
 end
 
 def prepare
--- src/ruby/www-steep/frontend/app.rb
+++ src/ruby/www-steep/frontend/app.rb
@@ -4,22 +4,43 @@ class MyApp < Ovto::App
   class State < Ovto::State
     item :keymap, default: ""
     item :result, default: ""
+    item :button_disabeld, default: true
+    item :button_css, default: "bg-red-100 font-semibold text-white py-2 px-10 rounded"
   end
 
   class Actions < Ovto::Actions
     def update_keymap(state:, keymap:)
+      keymap.length == 0 ? actions.disable_button(state: state) : actions.enable_button(state: state)
       { keymap: keymap }
     end
 
+    # In production
+    #   ENDPOINT = "https://xxxxxxxxx.execute-api.ap-northeast-1.amazonaws.com/prod/"
+    ENDPOINT = "http://localhost:4000/offline/"
+
     def steep_check(state:, button:)
       return if state.keymap.length == 0
-      button.disabled = true
-      Ovto.fetch('http://localhost:4000/offline/steep', 'POST', {keymap: state.keymap}).then{ |json|
+      actions.disable_button(state: state)
+      Ovto.fetch("#{ENDPOINT}steep", 'POST', {keymap: state.keymap}).then{ |json|
         actions.receive_action(json: json)
-        button.disabled = false
+        actions.enable_button(state: state)
       }.fail{ |e|  # Network error, 404 Not Found, JSON parse error, etc.
         p e
-        button.disabled = false
+      }
+      { result: "" }
+    end
+
+    def enable_button(state:)
+      {
+        button_disabeld: false,
+        button_css: "bg-red-700 font-semibold text-white py-2 px-10 rounded"
+      }
+    end
+
+    def disable_button(state:)
+      {
+        button_disabeld: true,
+        button_css: "bg-red-100 font-semibold text-white py-2 px-10 rounded"
       }
     end
 
@@ -40,14 +61,17 @@ class MyApp < Ovto::App
           }
         end
         o 'div', class: "my-4" do
-          o 'button', class: "bg-red-700 font-semibold text-white py-2 px-10 rounded", onclick: ->(e){ actions.steep_check(button: e.target) } do
-            'Check'
+          o 'button',
+            class: state.button_css,
+            disabled: state.button_disabeld,
+            onclick: ->(e){ actions.steep_check(button: e.target) } do
+              'Check'
+            end
           end
-        end
         o 'div', class: "my-4" do
           o 'div', 'Result:'
           o 'div', class: "bg-slate-400 font-mono leading-none p-4" do
-            o 'code' do
+            o 'pre' do
               state.result.split("\n").each do |line|
                 o 'text', line
                 o 'br'
