cf063a608026399e838857cb7154867d7927704e
HASUMI Hitoshi <hasumikin@gmail.com>
2023-01-21 14:31:14 +0900

    delete "bi_decl(bi_2pins_with_func" macro as it seems unnecessary

--- src/i2c.c
+++ src/i2c.c
@@ -38,7 +38,4 @@ I2C_gpio_init(uint8_t unit_num, uint32_t frequency, uint8_t sda_pin, uint8_t scl
   gpio_set_function(scl_pin, GPIO_FUNC_I2C);
   gpio_pull_up(sda_pin);
   gpio_pull_up(scl_pin);
-  if (sda_pin == 16 && scl_pin == 17) {
-    bi_decl(bi_2pins_with_func(I2C_SDA_PIN_16, I2C_SCL_PIN_17, GPIO_FUNC_I2C));
-  }
 }
