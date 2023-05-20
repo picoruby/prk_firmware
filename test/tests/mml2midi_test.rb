1c4d5bf6787a67a57291b183128deeab57d11b93
Toshio Maki <kirika.k2@gmail.com>
2023-05-20 23:45:00 +0900

    Uncomment test

--- test/tests/mml2midi_test.rb
+++ test/tests/mml2midi_test.rb
@@ -112,6 +112,8 @@ class Mml2midiTest < MrubycTestCase
 
   description "Super Mario A part"
   def mario_a_case
+    durations = [92, 92, 90, 116, 98, 116, 50]
+
     [
       "T200 L8 O5 Q4 e e r e r c e r g r4. > g r4.",
       "T200 L8 Q6 < c r4 > g r4 e r4 a r b r b- a r",
@@ -120,14 +122,15 @@ class Mml2midiTest < MrubycTestCase
       "T200 L8 Q6 r4   g g- f d+ r e r < c r c c r4.",
       "T200 L8 Q6 r4 > g g- f d+ r e r > g+ a < c r > a < c d",
       "T200 L8 Q6 r4 e-8 r4 d r4 c r r4 r2"
-    ].each do |measure|
+    ].each_with_index do |measure, index|
       duration = MML2MIDI.new.compile(measure) { |p, d| @data << [p, d] }
-      # assert_equal 60, duration
+      assert_equal durations[index], duration
     end
   end
 
   description "Super Mario B part"
   def mario_b_case
+    durations = [92, 92, 90, 80, 104, 80, 92]
     [
       "T200 L8 O3 Q4 d d r d r d d r g r4. > g r4.",
       "T200 L8 Q6 < g r4 e r4 c r4 f r g r g- f r",
@@ -136,20 +139,21 @@ class Mml2midiTest < MrubycTestCase
       "T200 L8 Q6 c r4 e r4 g < c r < f r f f r >> g r",
       "T200 L8 Q6 c r4 g r4 < c r > f r4 < c r4 > f r",
       "T200 L8 Q6 c r a- r4 b- r4 < c r4 > g g r c r"
-    ].each do |measure|
+    ].each_with_index do |measure, index|
       duration = MML2MIDI.new.compile(measure) { |p, d| @data << [p, d] }
-      # assert_equal 48, duration
+      assert_equal durations[index], duration
     end
   end
 
   description "Danger Zone"
   def danger_zone_case
+    durations = [320, 362]
     [
       "T120 Q6 L8 r O2 c d f g4 f4 < g a r4 a4. a r4 a a a4 a4 g a r4 a4. a r4 a a < c4 c4 > Q8 g1 Q6 g1 g a r4 a4. a r4 a a a4 a4 g a r4 a4. a r4 a a a4 a4",
       "T120 Q6 L8 r O2 c d f g4 f4 d << d r4 d4. d >> r c d f g4 f4 d << d r4 d4. d >> r c d f g4 f4 c4. > g < c2 d c4 > g < c4 c4 d4. d d2 r c d f g4 f4 d4. d d2 r c d f g4 f4"
-    ].each do |measure|
+    ].each_with_index do |measure, index|
       duration = MML2MIDI.new.compile(measure) { |p, d| @data << [p, d] }
-      # assert_equal 238, duration
+      assert_equal durations[index], duration
     end
   end
 end
