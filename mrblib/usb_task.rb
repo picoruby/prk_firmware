d8caf450a7fd7382435110b0db385ae5d8d5965a
HASUMI Hitoshi <hasumikin@gmail.com>
2023-06-16 19:12:59 +0900

    Upgrade PicoRuby
    - Stop comparing keymap.rb's timestamp as it didn't work well

--- mrblib/usb_task.rb
+++ mrblib/usb_task.rb
@@ -14,19 +14,9 @@ print "\e[2J\e[1;1H" # clear all & home
 
 Keyboard.mount_volume
 
-keymap_updated_at = -1
-
 while true
   USB.tud_task
-  if Keyboard.autoreload_ready?
-    if File.exist?("/keymap.rb")
-      unixtime = File::Stat.new("/keymap.rb").mtime.to_i
-      if unixtime != keymap_updated_at
-        Keyboard.restart
-        keymap_updated_at = unixtime
-      else
-        Keyboard.autoreload_off
-      end
-    end
+  if Keyboard.autoreload_ready? && File.exist?("/keymap.rb")
+    Keyboard.restart
   end
 end
